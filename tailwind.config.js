637343ea410a6d6d96213646452c151419a62452
jadeilton <jadeiltondm@gmail.com>
2025-09-23 09:05:54 -0300

    Finalizando parte do index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
  export default {
   content: ["./**/*.{html,js}"],
     theme: {
+
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
       extend: {
         backgroundImage: {
             "home": "url('/assets/capa.png')"
